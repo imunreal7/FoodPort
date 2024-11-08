409664646106cb9abc47f21bc910a69645882226
Aman Dubey <dubeyaman174@gmail.com>
2024-11-08 14:24:03 +0530

    Added Mongodb as the database

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,27 @@
 const express = require("express");
-var cors = require('cors')
-const app = express();
-app.use(cors())
+require("dotenv").config();
 
-const dotenv = require("dotenv").config()
+var cors = require("cors");
+const app = express();
 const products = require("./routes/productRoutes");
 const restaurants = require("./routes/restaurantRoutes");
+const mongoose = require("mongoose");
+const port = process.env.PORT;
 
-const port = process.env.PORT
+app.use(cors());
+
+mongoose
+    .connect(process.env.DB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((error) => console.error("MongoDB connection error:", error));
 
 app.use(express.json()); //used for parsing so that always the data return in json
 app.use("/api", products);
-app.use("/api",restaurants)
+app.use("/api", restaurants);
 app.listen(port, () => {
-  console.log(`App Running on port ${port}`);
+    console.log(`App Running on port ${port}`);
 });
+
