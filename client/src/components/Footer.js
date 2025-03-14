031781a656b41aa39806f10ab4e1fe9920e435ac
Aman Dubey <dubeyaman174@gmail.com>
2025-03-14 15:26:50 +0530

    Updated the details

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -100,7 +100,7 @@ const Footer = () => {
                     <hr className="my-6 border-gray-300 sm:mx-auto dark:border-gray-700 lg:my-9" />
                     <div className="sm:flex sm:items-center sm:justify-between">
                         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-                            © 2022{" "}
+                            © 2025{" "}
                             <Link to="/" className="hover:underline">
                                 FoodPort
                             </Link>
